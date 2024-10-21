ffecb1907c9a90852f76f793578dfd8e752515b1
Afrah Nakanwagi <anaiyosh@gmail.com>
2024-10-21 20:53:28 +0300

    added the test code for the factorial at last to confirm my code implementation

---
+++ test_factorial.py
@@ -0,0 +1,23 @@
+# test_factorial.py
+
+import unittest
+from factorial import factorial
+
+class TestFactorial(unittest.TestCase):
+
+    def test_factorial_of_zero(self):
+        self.assertEqual(factorial(0), 1)
+
+    def test_factorial_of_one(self):
+        self.assertEqual(factorial(1), 1)
+
+    def test_factorial_of_positive_number(self):
+        self.assertEqual(factorial(5), 120)
+        self.assertEqual(factorial(3), 6)
+
+    def test_factorial_of_negative_number(self):
+        with self.assertRaises(ValueError):
+            factorial(-5)
+
+if __name__ == '__main__':
+    unittest.main()
