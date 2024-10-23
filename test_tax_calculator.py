d66a64ff9c20bc64578779831585d0fd6efbab86
Afrah Nakanwagi <anaiyosh@gmail.com>
2024-10-23 22:23:47 +0300

    did the test first ans started by importing unittest since were going to use unittest'

---
+++ test_tax_calculator.py
@@ -0,0 +1,9 @@
+import unittest
+"""from tax_calculator import calculate_tax
+
+class TestTaxCalculator(unittest.TestCase):
+    def test_no_tax_for_earnings_below_threshold(self):
+        self.assertEqual(calculate_tax(10000), 0)
+
+if __name__ == '__main__':
+    unittest.main()"""
