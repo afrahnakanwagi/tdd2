6a4ab685a0db913ef3594a8177b67469ef064ae4
Afrah Nakanwagi <anaiyosh@gmail.com>
2024-10-23 22:20:11 +0300

    created a tax calculator file(directory) and the test tax calculator file to handle the tests

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,22 @@
 #pair: Afrah & Aisha
-def fibonacci_recursive(n):
+"""def fibonacci_recursive(n):
     if n == 0:
         return 0
     elif n == 1:
         return 1
     else:
-        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)"""
+
+# the code below
+def fibonacci(n):  
+    if n <= 0:  
+        return []  
+    elif n == 1:  
+        return [0]  
+    elif n == 2:  
+        return [0, 1]  
+    else:  
+        sequence = [0, 1]  
+        for i in range(2, n):  
+            sequence.append(sequence[-1] + sequence[-2])  
+        return sequence 
--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,6 +1,6 @@
 #pair: Aisha & Afrah
 
-from fibonacci import fibonacci_recursive
+"""from fibonacci import fibonacci_recursive
 
 def test_fibonacci_recursive():
     #numbers i expect for the first 10 indices
@@ -14,3 +14,18 @@ def test_fibonacci_recursive():
 # Runing our tests
 if __name__ == "__main__":
     test_fibonacci_recursive()
+"""
+import unittest
+import os
+from fibonacci import fibonacci 
+
+class TestFibonacci(unittest.TestCase):  
+    def test_fibonacci(self):  
+        self.assertEqual(fibonacci(0), []) 
+"""        self.assertEqual(fibonacci(1), [0])  
+        self.assertEqual(fibonacci(2), [0, 1])  
+        self.assertEqual(fibonacci(5), [0, 1, 1, 2, 3])  
+        self.assertEqual(fibonacci(10), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])"""  
+
+if __name__ == '__main__':  
+    unittest.main()
