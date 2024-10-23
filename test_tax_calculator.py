13577dcbb3b73a1bb00d3c4b9420bfb9ae965868
Afrah Nakanwagi <anaiyosh@gmail.com>
2024-10-23 22:25:47 +0300

    imported the calculator tax

--- test_tax_calculator.py
+++ test_tax_calculator.py
@@ -1,7 +1,7 @@
 import unittest
-"""from tax_calculator import calculate_tax
+from tax_calculator import calculate_tax
 
-class TestTaxCalculator(unittest.TestCase):
+"""class TestTaxCalculator(unittest.TestCase):
     def test_no_tax_for_earnings_below_threshold(self):
         self.assertEqual(calculate_tax(10000), 0)
 
